7beca8017deea53e9a81a133a165a4a95e22725f
Mohamed Sugal <sugaal@gmail.com>
2022-12-05 18:56:55 -0600

    fix uncontrolled state

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import { Container, Alert, Form, Button } from 'react-bootstrap';
+import { Container, Alert, Form, Button, Col, Row } from 'react-bootstrap';
 import axios from 'axios';
-import './App.css';
+import './styles.css';
 
 function App() {
-    const [fullName, setFullName] = useState('');
-    const [horoscopeSign, setHoroscopeSign] = useState('');
+    const [name, setName] = useState('');
+    const [horoscopeSign, setHoroscopeSign] = useState('aries');
 
     const flushData = (e: React.ChangeEvent<any>) => {
         e.preventDefault();
         let request = {
-            full_name: fullName,
+            name: name,
             horoscope_sign: horoscopeSign
         };
         axios
@@ -23,14 +23,18 @@ function App() {
             .catch((err) => {
                 console.log(err.response.request._response);
             });
-        setFullName('');
+        console.log(JSON.stringify(request));
+        setName('');
+        setHoroscopeSign('aries');
     };
 
     return (
         <Container>
             <h2 style={{ marginTop: '1em', marginBottom: '20px' }}>Horoscope</h2>
             <Alert variant="primary" className="col-md-6">
-                <p>This App Provides horoscope info for sun signs such as Lucky Number, Lucky Color, Mood, Color, Compatibility with other sun signs, description of a sign for that day, etc.</p>
+                {/* prettier-ignore */}
+                <p>This App Provides horoscope info for sun signs such as Lucky Number, Lucky Color, 
+                    Mood, Color, Compatibility with other sun signs, description of a sign for that day, etc.</p>
                 <hr />
                 <p>
                     POC: @mohamedsugal, @abdifatahdev
@@ -38,27 +42,33 @@ function App() {
                 </p>
             </Alert>
             <Form onSubmit={flushData} className="col-md-6">
-                <Form.Group className="formGroup">
-                    <Form.Label>Enter your name</Form.Label>
-                    <Form.Control required type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} />
-                </Form.Group>
-                <Form.Group className="formGroup">
-                    <Form.Label>What is your horoscope sign</Form.Label>
-                    <Form.Control as="select" value={horoscopeSign} onChange={(e) => setHoroscopeSign(e.target.value)} defaultValue="Libra">
-                        <option value="aries">Aries — Mar 21 - Apr 19</option>
-                        <option value="taurus">Taurus — Apr 20 - May 20 </option>
-                        <option value="gemini">Gemini — May 21 - Jun 20</option>
-                        <option value="cancer">Cancer — Jun 21 - Jul 22</option>
-                        <option value="leo">Leo — Jul 23 - Aug 22</option>
-                        <option value="virgo">Virgo — Aug 23 - Sep 22</option>
-                        <option value="libra">Libra — Sep 23 - Oct 22</option>
-                        <option value="scorpio">Scorpio — Oct 23 - Nov 21</option>
-                        <option value="sagittarius">Sagittarius — Nov 22 - Dec 21</option>
-                        <option value="capricorn">Capricorn — Dec 22 - Jan 19</option>
-                        <option value="aquarius">Aquarius — Jan 20 - Feb 18</option>
-                        <option value="pisces">Pisces — Feb 19 - Mar 20</option>
-                    </Form.Control>
-                </Form.Group>
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>Enter your name</Form.Label>
+                        <Form.Control required type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>What is your horoscope?</Form.Label>
+                        <Form.Control required as="select" value={horoscopeSign} onChange={(e) => setHoroscopeSign(e.target.value)}>
+                            <option value="aries">Aries — Mar 21 - Apr 19</option>
+                            <option value="taurus">Taurus — Apr 20 - May 20 </option>
+                            <option value="gemini">Gemini — May 21 - Jun 20</option>
+                            <option value="cancer">Cancer — Jun 21 - Jul 22</option>
+                            <option value="leo">Leo — Jul 23 - Aug 22</option>
+                            <option value="virgo">Virgo — Aug 23 - Sep 22</option>
+                            <option value="libra">Libra — Sep 23 - Oct 22</option>
+                            <option value="scorpio">Scorpio — Oct 23 - Nov 21</option>
+                            <option value="sagittarius">Sagittarius — Nov 22 - Dec 21</option>
+                            <option value="capricorn">Capricorn — Dec 22 - Jan 19</option>
+                            <option value="aquarius">Aquarius — Jan 20 - Feb 18</option>
+                            <option value="pisces">Pisces — Feb 19 - Mar 20</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Row>
+
                 <Button variant="primary" type="submit">
                     submit
                 </Button>
--- server/dist/index.js
+++ server/dist/index.js
@@ -4,12 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const port = 7777;
+const PORT = 7777;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.post('/user', (req, res) => {
     console.log(req.body);
 });
-app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
 });
