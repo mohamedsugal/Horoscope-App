9983eadb6cb9b431ac788867207205bc716058f6
Abdifatah Mohamed <mofaatah@gmail.com>
2022-12-10 18:33:43 -0600

    Changed GrpcRequest function into a class - stayled project like OOP way

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import { GrpcRequest } from './rpc-request';
 
+// Port number
 const PORT = 7777;
 const app: Express = express();
 
 app.use(express.json());
 
 app.post("/user", (req: Request, res: Response) => {
-  console.log(req.body);
+  const grpc_request = new GrpcRequest();
+  grpc_request.GetGrpcRequest(req.body["name"], req.body["horoscope_sign"])
+  
 });
 
 app.listen(PORT, () => {
